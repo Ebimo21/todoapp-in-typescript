e94b2335fd56eca5eca15226a5c728c02af3578c
ebimo21 <ebi4jah15@gmail.com>
2023-04-24 09:56:18 +0100

    Refactor

--- assets/app.js
+++ assets/app.js
@@ -1,64 +1,150 @@
 "use strict";
-const heading = document.getElementById("heading");
-const root = document.getElementById("root");
-const todoUL = document.getElementById("ulItem");
 const formEl = document.getElementById("todo-form");
+const todoUL = document.getElementById("ulItem");
+const todoInput = document.getElementById("todo-input");
 let listItemText;
-const headingOneEl = document.createElement("h1");
-const headingOneElTextNode = document.createTextNode("Welcome to my TodoList Application");
-headingOneEl.append(headingOneElTextNode);
-heading.append(headingOneEl);
-function initialize() {
-    initDatabase();
-    render();
-}
-const initDatabase = () => {
-    if (!localStorage.todolist) {
-        localStorage.todolist = JSON.stringify([]);
+let id = localStorage.id || 0;
+const todo = {
+    id: id,
+    name: todoInput.value,
+    completed: false,
+};
+const model = {
+    init: () => {
+        if (!localStorage.todos) {
+            localStorage.todos = JSON.stringify([]);
+        }
+        if (!localStorage.id) {
+            localStorage.id = JSON.stringify(0);
+        }
+    },
+    getTodos: () => {
+        return JSON.parse(localStorage.todos);
+    },
+    deleteTodo: (e) => {
+        const item = e.target.parentNode.innerText;
+        const remainingTodo = model.getTodos().filter((todo) => todo.name !== item);
+        localStorage.todos = JSON.stringify(remainingTodo);
+        console.log(remainingTodo);
     }
 };
-const getData = () => {
-    return JSON.parse(localStorage.todolist);
-};
-const setData = (item) => {
-    localStorage.todolist = JSON.stringify(item);
-};
-const handleRemoveAllChildNode = (parent) => {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
+const octopus = {
+    init: () => {
+        model.init();
+        view.init();
+    },
+    getData: () => {
+        return model.getTodos();
+    },
+    addData: (e) => {
+        e.preventDefault();
+        const output = model.getTodos();
+        const id = ++localStorage.id;
+        output.push(Object.assign(Object.assign({}, todo), { id: id, name: todoInput.value }));
+        localStorage.todos = JSON.stringify(output);
+        formEl.reset();
+        view.render();
+    },
+    updateData: (e) => {
+        const item = e.target.parentNode.innerText;
+        const modTodo = model.getTodos().map(todo => todo.name == item ? Object.assign(Object.assign({}, todo), { completed: !todo.completed }) : todo);
+        localStorage.todos = JSON.stringify(modTodo);
+    },
+    deleteData: (e) => {
+        model.deleteTodo(e);
+        view.render();
     }
 };
-const render = () => {
-    handleRemoveAllChildNode(todoUL);
-    getData().map((item, index) => {
-        const liEl = document.createElement("li");
-        const delBtn = document.createElement("button");
-        delBtn.addEventListener('click', (handleDeleteTodoItem)(item));
-        delBtn.innerText = "X";
-        delBtn.dataset.key = `${index}`;
-        liEl.dataset.key = `${index}`;
-        const listText = item;
-        liEl.append(listText);
-        liEl.append(delBtn);
-        todoUL.append(liEl);
-    });
+const view = {
+    init: () => {
+        view.render();
+        formEl.addEventListener("submit", octopus.addData);
+    },
+    render: () => {
+        view.handleRemoveAllChildNode(todoUL);
+        octopus.getData().map((item) => {
+            const liEL = document.createElement("li");
+            const btnEl = document.createElement("input");
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.checked = item.completed;
+            btnEl.type = "button";
+            btnEl.value = "X";
+            checkbox.addEventListener('change', octopus.updateData);
+            btnEl.addEventListener("click", octopus.deleteData);
+            liEL.innerText = item.name;
+            liEL.append(checkbox);
+            liEL.append(btnEl);
+            todoUL.append(liEL);
+        });
+    },
+    handleRemoveAllChildNode: (parent) => {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
 };
-const handleDeleteTodoItem = (todo) => {
-    return function () {
-        const item = getData().filter((item, index) => item !== todo);
-        setData(item);
-        render();
-    };
-};
-const handleAddTodo = (e) => {
-    e.preventDefault();
-    const formData = new FormData(formEl);
-    let data = getData();
-    const list = formData.get("todo-item");
-    data.push(list);
-    setData(data);
-    formEl.reset();
-    render();
-};
-formEl.addEventListener("submit", handleAddTodo);
-initialize();
+octopus.init();
+// const heading = document.getElementById("heading") as HTMLDivElement;
+// const root = document.getElementById("root") as HTMLDivElement;
+// const todoUL = document.getElementById("ulItem") as HTMLUListElement;
+// const formEl:HTMLFormElement = document.getElementById("todo-form") as HTMLFormElement
+// let listItemText: string[] ;
+// const headingOneEl = document.createElement("h1") as HTMLHeadingElement;
+// const headingOneElTextNode: Text = document.createTextNode("Welcome to my TodoList Application") ;
+// headingOneEl.append(headingOneElTextNode);
+// heading.append(headingOneEl);
+// function initialize():void{
+//     initDatabase()
+//     render()
+// }
+// const initDatabase =():void =>{
+//     if(!localStorage.todolist){
+//         localStorage.todolist = JSON.stringify([])
+//     }
+// }
+// const getData = ():string[] =>{
+//     return JSON.parse(localStorage.todolist)
+// }
+// const setData = (item:string[]):void =>{
+//     localStorage.todolist= JSON.stringify(item)
+// }
+// const handleRemoveAllChildNode = (parent:HTMLUListElement):void=>{
+//     while(parent.firstChild){
+//         parent.removeChild(parent.firstChild);
+//     }
+// }
+// const render = ():void =>{
+//     handleRemoveAllChildNode(todoUL)
+//     getData().map((item:string, index:number) =>{
+//         const liEl:HTMLLIElement = document.createElement("li");
+//         const delBtn:HTMLButtonElement = document.createElement("button");
+//         delBtn.addEventListener('click', (handleDeleteTodoItem)(item));
+//         delBtn.innerText = "X"
+//         delBtn.dataset.key = `${index}`;
+//         liEl.dataset.key = `${index}`;
+//         const listText:string = item
+//         liEl.append(listText)
+//         liEl.append(delBtn)
+//         todoUL.append(liEl)
+//     })
+// }
+// const handleDeleteTodoItem= (todo:string):VoidFunction =>{
+//         return function():void{
+//             const item:string[] = getData().filter( (item, index) => item !== todo)
+//             setData(item)
+//             render()
+//         }
+// }
+// const handleAddTodo =(e: any):void =>{
+//     e.preventDefault()
+//     const formData= new FormData(formEl) as any
+//     let data: string[] = getData();
+//     const list:string = formData.get("todo-item")
+//     data.push(list)
+//     setData(data)
+//     formEl.reset()
+//     render()    
+// }
+// formEl.addEventListener("submit", handleAddTodo)
+// initialize()
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,78 +1,192 @@
-const heading = document.getElementById("heading") as HTMLDivElement;
-const root = document.getElementById("root") as HTMLDivElement;
+const formEl:HTMLFormElement = document.getElementById("todo-form") as HTMLFormElement;
 const todoUL = document.getElementById("ulItem") as HTMLUListElement;
-const formEl:HTMLFormElement = document.getElementById("todo-form") as HTMLFormElement
-let listItemText: string[] ;
+const todoInput = document.getElementById("todo-input") as HTMLInputElement;
+let listItemText: string[];
+let id:number= localStorage.id || 0;
 
-const headingOneEl = document.createElement("h1") as HTMLHeadingElement;
-const headingOneElTextNode: Text = document.createTextNode("Welcome to my TodoList Application") ;
-headingOneEl.append(headingOneElTextNode);
-heading.append(headingOneEl);
-
-function initialize():void{
-    initDatabase()
-    render()
+interface Todo {
+    id: number,
+    name: string,
+    completed: boolean
 }
 
-const initDatabase =():void =>{
-    if(!localStorage.todolist){
-        localStorage.todolist = JSON.stringify([])
+const todo:Todo ={
+    id: id,
+    name: todoInput.value,
+    completed: false,
+}
+
+const model ={
+    init: ():void =>{
+        if(!localStorage.todos){
+            localStorage.todos = JSON.stringify([])
+        }
+
+        if(!localStorage.id){
+            localStorage.id =JSON.stringify(0)
+        }
+    },
+
+    getTodos : ():Todo[] =>{
+        return JSON.parse(localStorage.todos)
+    },
+
+    deleteTodo: (e:any)=>{
+        const item = e.target.parentNode.innerText
+        const remainingTodo =model.getTodos().filter((todo) =>todo.name !== item)
+        localStorage.todos = JSON.stringify(remainingTodo)
+        console.log(remainingTodo)
     }
 }
 
+const octopus ={
+    init: () =>{
+        model.init()
+        view.init()
+    },
 
-const getData = ():string[] =>{
-    return JSON.parse(localStorage.todolist)
-}
+    getData: ()=>{
+        return model.getTodos()
+    },
 
-const setData = (item:string[]):void =>{
-    localStorage.todolist= JSON.stringify(item)
-}
+    addData: (e:any)=>{
+        e.preventDefault()
+        const output = model.getTodos()
+        const id = ++localStorage.id
+        output.push({...todo, id: id, name:todoInput.value})
+        localStorage.todos = JSON.stringify(output)
+        formEl.reset()
+        view.render()
 
-const handleRemoveAllChildNode = (parent:HTMLUListElement):void=>{
-    while(parent.firstChild){
-        parent.removeChild(parent.firstChild);
+    },
+
+    updateData: (e:any)=>{
+        const item =  e.target.parentNode.innerText
+        const modTodo = model.getTodos().map(todo => 
+            todo.name ==item? {...todo, completed: !todo.completed}: todo)
+        localStorage.todos = JSON.stringify(modTodo)
+
+    },
+
+    deleteData: (e:any) =>{
+        model.deleteTodo(e)
+        view.render()
     }
 }
 
-const render = ():void =>{
-    handleRemoveAllChildNode(todoUL)
-    getData().map((item:string, index:number) =>{
-        const liEl:HTMLLIElement = document.createElement("li");
-        const delBtn:HTMLButtonElement = document.createElement("button");
+const view = {
+    init: ()=>{
+        view.render()
+        formEl.addEventListener("submit", octopus.addData)
 
-        delBtn.addEventListener('click', (handleDeleteTodoItem)(item));
-        delBtn.innerText = "X"
-        delBtn.dataset.key = `${index}`;
+    },
 
-        liEl.dataset.key = `${index}`;
-        const listText:string = item
-        liEl.append(listText)
-        liEl.append(delBtn)
-        todoUL.append(liEl)
-    })
-}
+    render: ()=>{
+        view.handleRemoveAllChildNode(todoUL)
+        octopus.getData().map((item:Todo) => {
+            const liEL = document.createElement("li")
+            const btnEl = document.createElement("input")
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox"
+            checkbox.checked = item.completed
+            btnEl.type = "button";
+            btnEl.value = "X";
+            checkbox.addEventListener('change', octopus.updateData)
 
-const handleDeleteTodoItem= (todo:string):VoidFunction =>{
-        return function():void{
-            const item:string[] = getData().filter( (item, index) => item !== todo)
-            setData(item)
-            render()
+            btnEl.addEventListener("click", octopus.deleteData)
+            liEL.innerText = item.name
+            liEL.append(checkbox)
+            liEL.append(btnEl)
+            todoUL.append(liEL)
+        })
+    },
+
+    handleRemoveAllChildNode :(parent:HTMLUListElement):void=>{
+            while(parent.firstChild){
+                parent.removeChild(parent.firstChild);
+            }
         }
 }
 
+octopus.init()
 
-const handleAddTodo =(e: any):void =>{
-    e.preventDefault()
-    const formData= new FormData(formEl) as any
-    let data: string[] = getData();
-    const list:string = formData.get("todo-item")
-    data.push(list)
-    setData(data)
-    formEl.reset()
-    render()    
-}
 
-formEl.addEventListener("submit", handleAddTodo)
 
-initialize()
+// const heading = document.getElementById("heading") as HTMLDivElement;
+// const root = document.getElementById("root") as HTMLDivElement;
+// const todoUL = document.getElementById("ulItem") as HTMLUListElement;
+// const formEl:HTMLFormElement = document.getElementById("todo-form") as HTMLFormElement
+// let listItemText: string[] ;
+
+// const headingOneEl = document.createElement("h1") as HTMLHeadingElement;
+// const headingOneElTextNode: Text = document.createTextNode("Welcome to my TodoList Application") ;
+// headingOneEl.append(headingOneElTextNode);
+// heading.append(headingOneEl);
+
+// function initialize():void{
+//     initDatabase()
+//     render()
+// }
+
+// const initDatabase =():void =>{
+//     if(!localStorage.todolist){
+//         localStorage.todolist = JSON.stringify([])
+//     }
+// }
+
+
+// const getData = ():string[] =>{
+//     return JSON.parse(localStorage.todolist)
+// }
+
+// const setData = (item:string[]):void =>{
+//     localStorage.todolist= JSON.stringify(item)
+// }
+
+// const handleRemoveAllChildNode = (parent:HTMLUListElement):void=>{
+//     while(parent.firstChild){
+//         parent.removeChild(parent.firstChild);
+//     }
+// }
+
+// const render = ():void =>{
+//     handleRemoveAllChildNode(todoUL)
+//     getData().map((item:string, index:number) =>{
+//         const liEl:HTMLLIElement = document.createElement("li");
+//         const delBtn:HTMLButtonElement = document.createElement("button");
+
+//         delBtn.addEventListener('click', (handleDeleteTodoItem)(item));
+//         delBtn.innerText = "X"
+//         delBtn.dataset.key = `${index}`;
+
+//         liEl.dataset.key = `${index}`;
+//         const listText:string = item
+//         liEl.append(listText)
+//         liEl.append(delBtn)
+//         todoUL.append(liEl)
+//     })
+// }
+
+// const handleDeleteTodoItem= (todo:string):VoidFunction =>{
+//         return function():void{
+//             const item:string[] = getData().filter( (item, index) => item !== todo)
+//             setData(item)
+//             render()
+//         }
+// }
+
+
+// const handleAddTodo =(e: any):void =>{
+//     e.preventDefault()
+//     const formData= new FormData(formEl) as any
+//     let data: string[] = getData();
+//     const list:string = formData.get("todo-item")
+//     data.push(list)
+//     setData(data)
+//     formEl.reset()
+//     render()    
+// }
+
+// formEl.addEventListener("submit", handleAddTodo)
+
+// initialize()
